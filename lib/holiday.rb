16d4ec78bb05781a5c383b7caba94766ba480897
Everett <everett.bowline@gmail.com>
2018-04-12 02:55:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,12 +61,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | season, holidays |
     puts "#{season.capitalize}:"
       holidays.each do | holiday, supplies |
-        puts " #{holiday.to_s.split("_").capitalize}: #{supplies}"
-       
+         holiday.to_s.split("_").capitalize: supplies
+             binding.pry
       end
   end
 end
-      binding.pry
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
