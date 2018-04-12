e885f9ab3d34cbcc99f5a9d524c624433379a142
Everett <everett.bowline@gmail.com>
2018-04-12 02:34:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,9 +73,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
   holiday_hash.map do | season, holidays |
-    holidays.map do | holiday, supply |
-    if supply == "BBQ"
+    holidays.map do | holiday, supplies |
+    if supplies == "BBQ"
       holiday
     end
     end
