299d8b4cdfb0f83e2621263d01038859933d85cd
Everett <everett.bowline@gmail.com>
2018-04-12 02:45:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-   if holiday_hash.keys[0] == :winter
+   if holiday_hash.key(:winter) == :winter
       array = holiday_hash[:winter].values.flatten
       array
     end
