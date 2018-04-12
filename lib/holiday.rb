352ba95c6b6352c3f28a0617823aa6482624613b
Everett <everett.bowline@gmail.com>
2018-04-12 02:26:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,6 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
       holidays.each do | holiday, supplies |
         holiday.to_s
-        binding.pry
         holiday.split("_")
         holiday.capitalize
         puts " #{holiday}: #{supplies}"
